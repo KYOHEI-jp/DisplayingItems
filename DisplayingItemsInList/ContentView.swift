ff0944321f38220b33e953aa74febfa3630a4962
KYOHEI-jp <kingdomhearts510@gmail.com>
2023-08-15 08:38:14 +0900

    Initial Commit

---
+++ DisplayingItemsInList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DisplayingItemsInList
+//
+//  Created by 渡辺恭平 on 2023/08/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DisplayingItemsInList/DisplayingItemsInListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayingItemsInListApp.swift
+//  DisplayingItemsInList
+//
+//  Created by 渡辺恭平 on 2023/08/15.
+//
+
+import SwiftUI
+
+@main
+struct DisplayingItemsInListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
